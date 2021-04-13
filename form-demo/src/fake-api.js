d111ca1be1510eda8bf39592e6b1db3dc6eb3e00
dmitrii <ffklffkl@gmail.com>
2021-04-13 21:42:54 +0300

    fix code style

--- form-demo/src/fake-api.js
+++ form-demo/src/fake-api.js
@@ -4,10 +4,10 @@ const validDomain = 'example.com';
 
 export default {
   async isEmailUsed(email) {
-    return timeout(200).then(() => usedEmails.includes(email))
+    return timeout(200).then(() => usedEmails.includes(email));
   },
   async isEmailDomainBanned(email) {
-    return timeout(200).then(() => !email.includes(validDomain))
+    return timeout(200).then(() => !email.includes(validDomain));
   },
   async register(credentials) {
     return timeout(200).then(() => { success: true });
